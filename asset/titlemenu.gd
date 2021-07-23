4b367aa1136ca5ce5059ad865ec1126495c9dc07
arvich <50786388+arvich@users.noreply.github.com>
2021-07-23 13:14:18 +0700

    Add files via upload

---
+++ asset/titlemenu.gd
@@ -0,0 +1,18 @@
+extends Node
+
+onready var admob = $AdMob
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	admob.load_banner()
+	admob.show_banner()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
